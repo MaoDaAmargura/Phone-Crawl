49eebf69af361763f4f46efc21c0e3d92b500dd5
Bucky24 <bucky24@Bucky24s-Mac-mini.local>
2010-01-20 18:31:08 -0800

    added basic character stats and a few get/set methods to Player class

--- Data/Player.m
+++ Data/Player.m
@@ -11,4 +11,74 @@
 
 @implementation Player
 
+-(Player *)init {
+	self = [super init];
+	if (self) {
+		fireResist = 0;
+		coldResist = 0;
+		lightningResist = 0;
+		poisonResist = 0;
+		darkResist = 0;
+		magicResist = 0;
+		
+		strength = 1;
+		willpower = 1;
+		dexterity = 1;
+		maxMana = 5;
+		mana = 5;
+		
+		aggroRange = 4;
+		
+		maxHp = 20;
+		hp = 20;
+		
+		maxShield = 10;
+		shield = 10;
+		
+		armorBody = nil;
+		armorHead = nil;
+		weaponLeft = nil;
+		weaponRight = nil;
+		return self;
+	}
+	return nil;
+}
+
+-(int)getStrength {
+	return strength;
+}
+
+-(int)getHp {
+	return hp;
+}
+
+-(int)getShield {
+	return shield;
+}
+
+-(Item *)getArmorBody {
+	return armorBody;
+}
+
+-(Item *)getArmorHead {
+	return armorHead;
+}
+
+-(Item *)getWeaponLeft {
+	return weaponLeft;
+}
+
+-(Item *)getWeaponRight {
+	return weaponRight;
+}
+
+-(void)setHp:(int)hp {
+	self.hp = hp;
+}
+	
+-(void)setShield:(int)shield {
+	self.shield = shield;
+}
+	 
+
 @end
