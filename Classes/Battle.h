0cd5945a3fa6f9be5f43907149327ce9ee566f0e
Bucky24 <bucky24@Bucky24s-Mac-mini.local>
2010-02-09 17:24:07 -0800

    Completed basic battle function.

--- Classes/Battle.h
+++ Classes/Battle.h
@@ -8,12 +8,21 @@
 
 #import <Foundation/Foundation.h>
 #import "Creature.h"
+#import "Util.h" // for elemType
+#import "Item.h"
+#include <stdlib.h>
 
+typedef enum {
+	QUICK_ATTACK,
+	STANDARD_ATTACK,
+	FATAL_SWING
+} Action;
 
 @interface Battle : NSObject {
 	
 }
 
-+ (void)doAttack:(Creature *)attacker :(Creature *)defender;
++ (void)doAttack:(Creature *)attacker :(Creature *)defender :(Action)action;
++ (float)getDamage:(Action)action;
 
 @end
