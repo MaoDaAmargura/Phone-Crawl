af2242855fa70825a76ee89fd003aba6ab2f3a4f
Austin Kelley <Austin@Austin-Kelleys-MacBook-Pro.local>
2010-03-09 18:35:46 -0800

    leveled buttons for spells

--- UI/Menu/SkillUpgradeViewController.m
+++ UI/Menu/SkillUpgradeViewController.m
@@ -4,6 +4,9 @@
 
 #import "Phone_CrawlAppDelegate.h"
 #import "UIImage+Overlay.h"
+#import <QuartzCore/QuartzCore.h>
+
+#import "Util.h"
 
 @interface SkillUpgradeViewController (Private)
 
@@ -37,6 +40,7 @@
 	[subspaceView addSubview:abilityView];
 	abilityView.hidden = YES;
 	[mainSegmentControl setSelectedSegmentIndex:0];
+	[self updateUI];
 }
 
 - (void)didReceiveMemoryWarning 
@@ -62,11 +66,33 @@
 #pragma mark -
 #pragma mark Control
 
+- (void) updateButton:(UIButton*)button withBackground:(NSString*)bkg number:(int)num
+{
+	UIImage *bkgrnd = [UIImage imageNamed:bkg];
+	UIImage *lvlImg = [UIImage imageNamed:[NSString stringWithFormat:@"Numeral%d-Normal.png", num]];
+	UIImage *icon = [bkgrnd overlayedWithImage:lvlImg];
+	[button setImage:icon forState:UIControlStateNormal];
+	CALayer *layer = [button layer];
+	[layer setMasksToBounds:YES];
+	[layer setCornerRadius:10.0];
+}
+
 - (void) updateUI
 {
 	Abilities *playerAbils = [player abilities];
-	UIImage *flameIcon = [[UIImage imageNamed:@"bg-fire.png"] overlayedWithImage:[UIImage imageNamed:[NSString stringWithFormat:@"Numeral%d-Normal", playerAbils.spellBook[0]]]];
-	[flameSkillButton setImage:flameIcon forState:UIControlStateNormal];
+	
+	[self updateButton:flameSkillButton withBackground:@"bg-fire.png" number:playerAbils.spellBook[FIREDAMAGE]];
+	[self updateButton:frostSkillButton withBackground:@"bg-cold.png" number:playerAbils.spellBook[COLDDAMAGE]];
+	[self updateButton:shockSkillButton withBackground:@"bg-lightning.png" number:playerAbils.spellBook[LIGHTNINGDAMAGE]];
+	[self updateButton:erodeSkillButton withBackground:@"bg-poison.png" number:playerAbils.spellBook[POISONDAMAGE]];
+	[self updateButton:drainSkillButton withBackground:@"bg-dark.png" number:playerAbils.spellBook[DARKDAMAGE]];
+	
+	[self updateButton:burnSkillButton withBackground:@"bg-fire.png" number:playerAbils.spellBook[FIRECONDITION]];
+	[self updateButton:freezeSkillButton withBackground:@"bg-cold.png" number:playerAbils.spellBook[COLDCONDITION]];
+	[self updateButton:purgeSkillButton withBackground:@"bg-lightning.png" number:playerAbils.spellBook[LIGHTNINGCONDITION]];
+	[self updateButton:poisonSkillButton withBackground:@"bg-poison.png" number:playerAbils.spellBook[POISONCONDITION]];
+	[self updateButton:confuseSkillButton withBackground:@"bg-dark.png" number:playerAbils.spellBook[DARKCONDITION]];
+	
 }
 
 #pragma mark -
