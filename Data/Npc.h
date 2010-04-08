9439b77da8231e2b15a3bf7429e97a6a6267d092
Bucky24 <bucky24@Bucky24s-Mac-mini.local>
2010-04-07 22:13:30 -0700

    commented Npc.h and Npc.m

--- Data/Npc.h
+++ Data/Npc.h
@@ -5,38 +5,58 @@
 //  Created by Bucky24 on 4/4/10.
 //  Copyright 2010 __MyCompanyName__. All rights reserved.
 //
+//	This file contains the main NPC class, which is designed to hold the data
+//	required for complete interaction for an NPC, and the Dialog and Response
+//	helper classes
 
 #import <Foundation/Foundation.h>
 #import "Critter.h"
 
 
+
+// interface for Response
+// Responses have a string of dialog text,
+// a number that points to a response from the NPC
+// and an optional function pointer
 @interface Response : NSObject {
 	NSString *dialog;
 	int pointsTo;
 	SEL callfunc;
 }
 
+// getter and setter methods
 @property (retain) NSString *dialog;
 @property int pointsTo;
 @property SEL callfunc;
 
+// init methods
 -(id) initWithDialog:(NSString *)d pointsTo:(int) points func:(SEL) func;
 
 @end
 
+// interface for Dialog
+// Dialogs have a dialog string
+// and a linked list of possible responses
 @interface Dialog : NSObject {
 	NSString *dialog;
 	NSMutableArray *responses;
 }
 
+// getter and setter methods
 @property (retain) NSString *dialog;
 @property (nonatomic, retain) NSMutableArray *responses;
 
+// init methods (addResponse is only called during initialization)
 -(id) initWithDialog:(NSString *)d;
 -(void) addResponse:(Response *)r;
 
 @end
 
+// interface for Npc
+// Npc has a linked list of dialogs that the NPC can say
+// an opening dialog, which the NPC says upon starting conversation
+// a pointer to the player object (needed for healing player, selling items, ect)
+// and a pointer to the current dialog object in use in the conversation
 @interface Npc : NSObject {
 	NSMutableArray *dialogs;
 	Dialog *opening;
@@ -44,11 +64,14 @@
 	Dialog *current;
 }
 
+// getter and setter methods
 @property (retain) NSMutableArray *dialogs;
 @property (retain) Dialog *current;
 @property (nonatomic, copy) Dialog *opening;
 
+// init methods
 -(id) initWithCritter:(Critter *)c;
+// given a chosen response, moves the conversation to the required dialog
 -(void) changeDialog:(Response *)r;
 
 @end
